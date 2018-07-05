033f47bb1a09132ff7c9aa3a2c1b17c043482a01
Junior <junior4g4@gmail.com>
2018-07-05 20:59:26 -0300

    Init

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+        <Main icon="home" title="Inicio"
+            subtitle="Segundo Projeto do capítulo de React.">
+            <div className="display-4">Bem vindo!!</div>
+            <hr/>
+            <p className="mb-0">
+            Sistema para exemplificar 
+            </p>
+        </Main>  
